0f485294db1723942ef576d86388bb14a9e39914
Hira <hira@Hiras-MacBook-Pro.local>
2024-01-23 12:26:23 +0600

    Order Service name bug fix  Added

--- opentelemetry.js
+++ opentelemetry.js
@@ -1,21 +1,26 @@
-// opentelemetry.js
-
 const { NodeTracerProvider } = require('@opentelemetry/node');
 const { registerInstrumentations } = require('@opentelemetry/instrumentation');
 const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express');
 const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
 const { BatchSpanProcessor } = require('@opentelemetry/tracing');
+const { Resource } = require('@opentelemetry/resources');
+const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
 
 function configureOpenTelemetry() {
   // Create a tracer provider and register the Express instrumentation
-  const provider = new NodeTracerProvider();
+  const provider = new NodeTracerProvider({
+    resource: new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: 'order-service',
+      // Add other resource attributes as needed
+    }),
+  });
   provider.register();
 
   // Configure and register Jaeger exporter
   const exporter = new JaegerExporter({
-    serviceName: 'your-express-app',
+    serviceName: 'order-service',
     agentHost: 'localhost', // Change this to your Jaeger host
-    agentPort: 6831, // Change this to your Jaeger port
+    agentPort: 16686, // Change this to your Jaeger port
   });
 
   // Use BatchSpanProcessor
